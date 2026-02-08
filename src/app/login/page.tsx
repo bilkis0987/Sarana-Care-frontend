27c4f211398cb2e02ba3b3cb7f7b1db5bbbbe699
Frenky Jullyan <rayaeshalrayda@gmail.com>
2026-02-08 22:15:50 +0700

    chore(frontend): init layout, home, login UI

---
+++ src/app/login/page.tsx
@@ -0,0 +1,10 @@
+export default function LoginPage() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <input placeholder="Email" />
+      <input type="password" placeholder="Password" />
+      <button>Masuk</button>
+    </div>
+  )
+}
